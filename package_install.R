dfaa397b1cb1c463969980ed453292a18d9bc8c7
Sarina Singh Khaira <sarina.singhkhaira@fss.scot>
2023-09-18 17:41:43 +0100

    add code to install packages

---
+++ package_install.R
@@ -0,0 +1,36 @@
+
+
+
+
+
+install.packages.scots <- function(pckg_name){
+  install.packages(pckg_name, type="win.binary", repos=NULL, contriburl ="file://s1428a/R_Packages/R_4_2_2_Packages")
+}
+
+install.packages.scots("purrr")
+
+dashboard_pckgs <- c(
+  
+  "shiny",
+  "shinydashboard",
+  "dashboardthemes",
+  "shinyWidgets",
+  "purrr",
+  "shinyBS",
+  "readxl",
+  "plotly",
+  "tidyverse",
+  "janitor",
+  "htmlwidgets",
+  "lubridate",
+  "scales",
+  "sgplot",
+  "shinyalert"
+
+)
+
+
+pckgs_to_install <- dashboard_pckgs[!dashboard_pckgs %in% installed.packages()]
+
+
+if(length(pckgs_to_install) > 0){purrr::map(pckgs_to_install, install.packages.scots)}
